57a342630ef345a08e982613d7e684395f277b54
Tisza <Logangirvin@hotmail.com>
2013-01-21 13:49:31 -0800

    Added independent feed motor (right joystick)

--- Shooter_Test/robot.py
+++ Shooter_Test/robot.py
@@ -1,12 +1,15 @@
 import wpilib
 
 lstick = wpilib.Joystick(1)
+rstick = wpilib.Joystick(2)
 
 Motor = wpilib.Jaguar(1)
 Motor2 = wpilib.Jaguar(2)
 
 motorValue = 0
 motorOld = 0
+endMotorValue = 0
+endMotorOld = 0
 
 def CheckRestart():
     if lstick.GetRawButton(10):
@@ -40,31 +43,46 @@ class MyRobot(wpilib.IterativeRobot):
         CheckRestart()
         global motorValue
         global motorOld
+        global endMotorValue
+        global endMotorOld
 
         if lstick.GetRawButton(0):              #Trigger sets shooter to 100%
+                endMotorValue = -1
+        if rstick.GetRawButton(0):
                 motorValue = -1
         if lstick.GetRawButton(1):              #Button 2 sets to magic number
-                motorValue = -0.47
+                endMotorValue = -0.47
         else:                                   #Joystick y axis controls incrementally
                 if lstick.GetY() > 0.05 or lstick.GetY() < -0.05:
-                        motorValue += lstick.GetY()/100
-                
+                        endMotorValue += lstick.GetY()/100
+        if rstick.GetRawButton(1):
+                motorValue = -0.47
+        else:
+                if rstick.GetY() > 0.05 or rstick.GetY() < -0.05:
+                        motorValue += rstick.GetY()/100
         if abs(lstick.GetX() > 0.5) or lstick.GetRawButton(3):            #Shake joystick x-axis or press button 3 to e-stop motors
+                endMotorValue = 0
+        if abs(rstick.GetX() > 0.5) or rstick.GetRawButton(3):
                 motorValue = 0
-
-                
+        
         if motorValue > 1.0:                    #Keeps increments at or under +/-100%
                 motorValue = 1.0
         elif motorValue < -1.0:
                 motorValue = -1.0
-
+        if endMotorValue > 1.0:
+                endMotorValue = 1.0
+        elif motorValue < -1.0:
+                endMotorValue = -1.0
+        
         Motor.Set(motorValue)
-        Motor2.Set(motorValue)
-        if motorValue != motorOld:              #prints motor value
-            print(motorValue)
+        Motor2.Set(endMotorValue)
+        if endMotorValue != endMotorOld:              #prints motor value
+            print("End Motor:"+endMotorValue)
+            endMotorOld = endMotorValue
+        if motorValue != motorOld:
+            print("Feed Motor:"+motorValue)
             motorOld = motorValue
         
-  
 
 def run():
     robot = MyRobot()
