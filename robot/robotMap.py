b7a44a1b97d1463553f9a84c946be7f4f5a09903
Tisza <Logangirvin@hotmail.com>
2013-02-09 19:01:46 -0800

    standardized wiring

--- robot/robotMap.py
+++ robot/robotMap.py
@@ -1,22 +1,25 @@
-pistonForwardChannel = 0
-pistonReverseChannel = 0
+pistonForwardChannel = 1
+pistonReverseChannel = 2
+
+lockPistonForward = 3
+lockPistonReverse = 4
 
 forwardShooterChannel = 0
 backShooterChannel = 0
 
-shootEncoder1 = 0
-shootEncoder2 = 0
+shootEncoder1 = 5
+shootEncoder2 = 6
 
-leftDriveEncoder1 = 0
-leftDriveEncoder2 = 0
+leftDriveEncoder1 = 3
+leftDriveEncoder2 = 4
 
-rightDriveEncoder1 = 0
-rightDriveEncoder2 = 0
+rightDriveEncoder1 = 1
+rightDriveEncoder2 = 2
 
-drumEncoder1 = 0
-drumEncoder2 = 0
+drumEncoder1 = 9
+drumEncoder2 = 10
 
 liftMotorChannel = 0
 
-pressureSwitch = 0 
+pressureSwitch = 0
 compressorSpike = 0
