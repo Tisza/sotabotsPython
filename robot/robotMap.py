407948b9ed3d6c6d241fd5b540609e2738387e3e
Tisza <Logangirvin@hotmail.com>
2013-02-10 00:21:26 -0800

    end of night status

--- robot/robotMap.py
+++ robot/robotMap.py
@@ -4,8 +4,11 @@ pistonReverseChannel = 2
 lockPistonForward = 3
 lockPistonReverse = 4
 
-forwardShooterChannel = 0
-backShooterChannel = 0
+forwardShooterChannel = 3
+backShooterChannel = 4
+
+leftDrive = 1
+rightDrive = 2 
 
 shootEncoder1 = 5
 shootEncoder2 = 6
@@ -19,7 +22,7 @@ rightDriveEncoder2 = 2
 drumEncoder1 = 9
 drumEncoder2 = 10
 
-liftMotorChannel = 0
+liftMotorChannel = 6
 
-pressureSwitch = 0
-compressorSpike = 0
+pressureSwitch = 14
+compressorSpike = 1
