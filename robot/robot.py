c99821c3f5c848a3974c97f417a9802beaffc7ca
Tisza <Logangirvin@hotmail.com>
2013-02-10 20:38:46 -0800

    Loader Fix(?) + Control Mapping

--- robot/robot.py
+++ robot/robot.py
@@ -16,7 +16,8 @@ forwardShooter = wpilib.Jaguar(robotMap.forwardShooterChannel)
 backShooter = wpilib.Jaguar(robotMap.backShooterChannel)
 
 #loader piston 
-loader = wpilib.DoubleSolenoid(robotMap.pistonForwardChannel, robotMap.pistonReverseChannel )
+loader1 = wpilib.Solenoid(robotMap.pistonForwardChannel)
+loader2 = wpilib.Solenoid(robotMap.pistonReverseChannel)
 
 #lift motor
 liftMotor = wpilib.Jaguar(robotMap.liftMotorChannel)
@@ -26,12 +27,14 @@ compressor = wpilib.Compressor(robotMap.pressureSwitch,robotMap.compressorSpike)
 
 #encoders
 shootEncoder = wpilib.Encoder( robotMap.shootEncoder1 , robotMap.shootEncoder2 , True, wpilib.CounterBase.k4X)
+feedEncoder = wpilib.Encoder(robotMap.feedEncoder1, robotMap.feedEncoder2, True, wpilib.CounterBase.k4X)
 leftDriveEncoder = wpilib.Encoder( robotMap.leftDriveEncoder1 , robotMap.leftDriveEncoder2 , True, wpilib.CounterBase.k4X)
 rightDriveEncoder = wpilib.Encoder( robotMap.rightDriveEncoder1 , robotMap.rightDriveEncoder2 , True, wpilib.CounterBase.k4X)
 drumEncoder = wpilib.Encoder( robotMap.drumEncoder1 , robotMap.drumEncoder2 , True, wpilib.CounterBase.k4X)
 
 #shooter motor value
-motorValue = 0.0
+frontValue = 0
+backValue = 0
 
 #drive train
 drive = wpilib.RobotDrive(leftMotor,rightMotor)
@@ -74,34 +77,63 @@ class MyRobot(wpilib.IterativeRobot):
         dog.SetExpiration(0.25)
         compressor.Start()
         shootEncoder.Start()
-
-        global motorValue
+        feedEncoder.Start()
 
     def TeleopPeriodic(self):
         self.GetWatchdog().Feed()
         CheckRestart()
+        global frontValue
+        global backValue
         
         # Drive control
         drive.ArcadeDrive(lstick)
 
 	#shooter controls
-        forwardShooter.Set((rstick.GetThrottle()-1)/2) #Richs prefers a increase decrease button on 10/11 and
-        backShooter.Set((rstick.GetThrottle()-1)/2) #on the other side of rightJoystick... whatever values those are
+        if rstick.GetRawButton(11):
+            frontValue+=1
+            if frontValue > 1:
+                frontValue = 1
+            print("Front: "+str(int(frontValue*100))+"%")
+            wpilib.Timer.Delay(1)
+        if rstick.GetRawButton(10):
+            frontValue-=1
+            if frontValue < 0:
+                frontValue = 0
+            print("Front: "+str(int(backValue*100))+"%")
+            wpilib.Timer.Delay(1)
+        if rstick.GetRawButton(6):
+            backValue+=1
+            if backValue > 1:
+                backValue = 1
+            print("Back: "+str(int(frontValue*100))+"%")
+            wpilib.Timer.Delay(1)
+        if rstick.GetRawButton(7):
+            backValue-=1
+            if backValue < 0:
+                backValue = 0
+            print("Back: "+str(int(backValue*100))+"%")
+            wpilib.Timer.Delay(1)
+        forwardShooter.Set(frontValue)
+        backShooter.Set(backValue)
         
         #Shooter piston control
-        if rstick.GetTrigger(): 
-        	loader.Set( wpilib.DoubleSolenoid.Value.kForward )
-        	wpilib.Timer.Delay( 1 )
-        	loader.Set( wpilib.DoubleSolenoid.Value.kReverse )
+        if rstick.GetTrigger():
+            #loader.Set( wpilib.DoubleSolenoid.Value.kForward )
+            loader1.Set(True )
+            loader2.Set(False )
+            wpilib.Timer.Delay( 1 )
+            #loader.Set( wpilib.DoubleSolenoid.Value.kReverse )
+            loader1.Set(False )
+            loader2.Set(True )
         	
         #lift controls
-        if rstick.GetRawButton(10):
+        if lstick.GetRawButton(10):
         	liftMotor.Set(-0.4)
-        elif rstick.GetRawButton(11):
+        elif lstick.GetRawButton(11):
         	liftMotor.Set(0.4)
         	
         #prints encoder value
-        print(shootEncoder.GetRate())
+        print("Shooter: "+str(shootEncoder.GetRate())+" Feeder: "+str(feedEncoder.GetRate()))
         
 
 def run():
