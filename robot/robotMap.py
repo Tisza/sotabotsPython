a02f48d6ea9f097de867cdd407b8c12910024985
Tisza <Logangirvin@hotmail.com>
2013-02-10 20:39:21 -0800

    Some Housecleaning

--- robot/robotMap.py
+++ robot/robotMap.py
@@ -1,17 +1,28 @@
+#Solenoids
+
 pistonForwardChannel = 1
 pistonReverseChannel = 2
 
 lockPistonForward = 3
 lockPistonReverse = 4
 
+#pmws
+
 forwardShooterChannel = 3
 backShooterChannel = 4
 
 leftDrive = 1
-rightDrive = 2 
+rightDrive = 2
+
+liftMotorChannel = 6
+
+#Digital I/Os
 
 shootEncoder1 = 5
 shootEncoder2 = 6
+
+feedEncoder1 = 7
+feedEncoder2 = 8
 
 leftDriveEncoder1 = 3
 leftDriveEncoder2 = 4
@@ -22,7 +33,8 @@ rightDriveEncoder2 = 2
 drumEncoder1 = 9
 drumEncoder2 = 10
 
-liftMotorChannel = 6
-
 pressureSwitch = 14
+
+#Relays 
+
 compressorSpike = 1
