d901f9e7bf3e895d34b9f7d967808b5ac271a29b
Tisza <Logangirvin@hotmail.com>
2013-02-19 00:00:14 -0800

    robot updates

--- robot/robot.py
+++ robot/robot.py
@@ -27,8 +27,8 @@ hopper1 = wpilib.Solenoid(robotMap.hopperForwardChannel)
 hopper2 = wpilib.Solenoid(robotMap.hopperReverseChannel)
 
 #magic Jacks
-magic1 = wpilib.Solenoid(robotMap.magicJackUp)
-magic2 = wpilib.Solenoid(robotMap.magicJackDown)
+#magic1 = wpilib.Solenoid(robotMap.magicJackUp)
+#magic2 = wpilib.Solenoid(robotMap.magicJackDown)
 
 #Dawg control
 dawg1 = wpilib.Solenoid(robotMap.dawgLock)
@@ -59,8 +59,10 @@ drumEncoder = wpilib.Encoder( robotMap.drumEncoder1 , robotMap.drumEncoder2 , Tr
 frontValue = 0
 backValue = 0
 direction = 1
+lsense = (lstick.GetThrottle() -1)*(-0.5)
+rsense = (rstick.GetThrottle() -1)*(-0.5)
 fire = False 
-jackItUp = False
+#jackItUp = False
 doggie = False
 hop = False
 dire=False 
@@ -79,6 +81,7 @@ filterEncoder = aimFilter(100)
 timer = wpilib.Timer()
 start = 0
 start2 = 0
+start3 = 0
 
 def CheckRestart():
     if lstick.GetRawButton(10):
@@ -113,10 +116,10 @@ class MyRobot(wpilib.IterativeRobot):
         timer.Start()
         
         #starting positions
-        magic1.Set(True)
-        magic2.Set(False)
-        dawg1.Set(False)
-        dawg2.Set(True)
+        #magic1.Set(True)
+        #magic2.Set(False)
+        #dawg1.Set(False)
+        #dawg2.Set(True)
         
     def TeleopPeriodic(self):
         self.GetWatchdog().Feed()
@@ -125,40 +128,47 @@ class MyRobot(wpilib.IterativeRobot):
         global backValue
         global fire
         global start
-        global jackItUp
+        #global jackItUp
         global armValue
         global doggie
         global start2
         global hop
         global direction
         global dire
+        global lsense
+        global rsense
+        global start3
+        
+        #sensitivity
+        lsense = (lstick.GetThrottle() -1)*(-0.5)
+        rsense = (rstick.GetThrottle() -1)*(-0.5)
         
         # Drive control
-        if lstick.GetRawButton(3) and dire==False:
+        if lstick.GetRawButton(2) and dire==False:
             direction = direction*-1
             dire=True
-        if not lstick.GetRawButton(3) and dire==True:
+        if not lstick.GetRawButton(2) and dire==True:
             dire=False
-        drive.ArcadeDrive(lstick.GetY()*direction,lstick.GetX()*.8)
+        drive.ArcadeDrive(lstick.GetY()*direction*lsense,lstick.GetX()*.8*lsense)
 
 		#shooter controls
         if rstick.GetRawButton(11):				#right button 11 increments FRONT by 1%
-            frontValue+=.01
+            frontValue+=.05*rsense
             if frontValue > 1:
                 frontValue = 1
             print("Front: "+str(int(frontValue*100))+"%")
         elif rstick.GetRawButton(10):				#right button 10 decrements FRONT to by 1%
-            frontValue-=.01
+            frontValue-=.05*rsense
             if frontValue < 0:
                 frontValue = 0
             print("Front: "+str(int(frontValue*100))+"%")
         if rstick.GetRawButton(6):				#right button 6 increments BACK by 1%
-            backValue+=.01
+            backValue+=.05*rsense
             if backValue > 1:
                 backValue = 1
             print("Back: "+str(int(backValue*100))+"%")
         elif rstick.GetRawButton(7):				#right button 7 decrements BACK by 1% 
-            backValue-=.01
+            backValue-=.05*rsense
             if backValue < 0:
                 backValue = 0
             print("Back: "+str(int(backValue*100))+"%")
@@ -209,7 +219,7 @@ class MyRobot(wpilib.IterativeRobot):
             fire = False
             
         #hopper piston control
-        if rstick.GetRawButton(3) and hop==False:
+        if rstick.GetRawButton(2) and hop==False:
             hop = True
             start2 = timer.Get()
         if start2 != 0:
@@ -218,41 +228,50 @@ class MyRobot(wpilib.IterativeRobot):
         else:
             hopper1.Set(True)
             hopper2.Set(False)
-        iif (timer.Get() > start2+.2) and magic1.Get()==True:  #.2 second interval for hopper piston
+        iif (timer.Get() > start2+.2) #and magic1.Get()==True:  #.2 second interval for hopper piston
             start2 = 0
-        if (timer.Get() > start2+.5) and magic1.Get()==False:
-            start2 = 0
-        if start == 0 and not rstick.GetRawButton(3):
+        #if (timer.Get() > start2+.5) and magic1.Get()==False:
+            #start2 = 0
+        if start == 0 and not rstick.GetRawButton(2):
             hop = False
             
         #Magic Jacks
-        if lstick.GetRawButton(8) and lstick.GetRawButton(9) and jackItUp==False:	#button 8 and 9 at same time to toggle jacks
-            magic1.Set(not magic1.Get())
-            magic2.Set(not magic2.Get())
-            jackItUp=True
-        if (not lstick.GetRawButton(8) or not lstick.GetRawButton(9)) and jackItUp==True:
-            jackItUp=False
+        #if lstick.GetRawButton(8) and lstick.GetRawButton(9) and jackItUp==False:	#button 8 and 9 at same time to toggle jacks
+        #    magic1.Set(not magic1.Get())
+        #    magic2.Set(not magic2.Get())
+        #    jackItUp=True
+        #if (not lstick.GetRawButton(8) or not lstick.GetRawButton(9)) and jackItUp==True:
+        #    jackItUp=False
         
         #lift controls
         if lstick.GetRawButton(10):
             if liftTopSwitch.Get()==1:
-                drumMotor.Set(0.5)
+                drumMotor.Set(lsense)
             else:
                 drumMotor.Set(0)
+            if dawg1.Get()==True:
+                start3 = timer.Get()
+            if timer.Get() > start3+.05:
+                dawg1.Set(False)
+                dawg2.Set(True)
+                start3 = 0
         elif lstick.GetRawButton(11):
             if liftBottomSwitch.Get()==1:
-                drumMotor.Set(-0.5)
+                drumMotor.Set(-lsense)
             else:
                 drumMotor.Set(0)
+            if dawg1.Get()==False:
+                dawg1.Set(True)
+                dawg2.Set(False)
         else:
             drumMotor.Set(0)
             
        #arm controls
         if lstick.GetRawButton(6):
-            weeWooMotor.Set(.5)
+            weeWooMotor.Set(lsense)
         elif lstick.GetRawButton(7):
             if armLimitSwitch.Get()==1:
-                weeWooMotor.Set(-.5)
+                weeWooMotor.Set(-lsense)
             else:
                 weeWooMotor.Set(0)
         else:
@@ -263,6 +282,7 @@ class MyRobot(wpilib.IterativeRobot):
             dawg1.Set(not dawg1.Get())
             dawg2.Set(not dawg2.Get())
             doggie = True
+            print("Dog Enabled:"+str(dawg1.Get()))
         if not lstick.GetTrigger() and doggie == True:
             doggie = False
         	
